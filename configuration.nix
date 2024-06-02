93b037fd377e3e41791ff784227f271166705907
Ivan Kostrubin <7spawn@gmail.com>
2024-06-02 20:20:51 +0300

    awesome-git-now-working-as-should

--- configuration.nix
+++ configuration.nix
@@ -17,8 +17,10 @@
           repo = "awesome";
           rev = "3a542219f3bf129546ae79eb20e384ea28fa9798";
           sha256 = "4z3w6iuv+Gw2xRvhv2AX4suO6dl82woJn0p1nkEx3uM=";
+
         };
         patches = [];
+        buildInputs = (old.buildInputs or []) ++ [ pkgs.gobject-introspection pkgs.gtk3 pkgs.playerctl ];
       });
     })
   ];
